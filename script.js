b840c5a6b144945f177b55702b36ee3e2cd16afe
jessyan88 <Jessian.dev23@gmail.com>
2023-11-19 23:08:42 +0800

    added js for shuffling carousel of home page images

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ document.addEventListener('DOMContentLoaded', function () {
   // Initial slide display
   showSlide(currentIndex);
 });
+
+// let currentIndex = 0;
+//   const items = document.querySelector('.homepage-img-container');
+//   const totalItems = document.querySelectorAll('.homepage-img').length;
+
+//   function showNext() {
+//     if (currentIndex < totalItems - 1) {
+//       currentIndex++;
+//     } else {
+//       currentIndex = 0;
+//     }
+//     updateCarousel();
+//   }
+
+//   function updateCarousel() {
+//     const newTransformValue = -currentIndex * 100 + '%';
+//     items.style.transform = 'translateX(' + newTransformValue + ')';
+//   }
+
+//   setInterval(showNext, 3000); // Change image every 3 seconds (adjust as needed)
