cae0ba7bd117474c5dd72ad829792b75c7c0c3f2
jessyan88 <Jessian.dev23@gmail.com>
2023-11-21 09:48:48 +0800

    added js on hamburger menu

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const carousel = document.querySelector('.pg-txt-container');
-//     const images = document.querySelectorAll('.pg-img');
-  
-//     let currentIndex = 0;
-  
-//     function showImage(index) {
-//       const transformValue = -index * 100 + '%';
-//       carousel.style.transform = 'translateX(' + transformValue + ')';
-//     }
-  
-//     function nextImage() {
-//       currentIndex = (currentIndex + 1) % images.length;
-//       showImage(currentIndex);
-//     }
-  
-//     setInterval(nextImage, 5000);
-//   });
-
-
-// DOMContentLoaded - loads all the elements in the DOM before you can access any elements
-  
 document.addEventListener('DOMContentLoaded', function () {
   let currentIndex = 0;
   const slides = document.querySelectorAll('.homepage-img');
@@ -41,22 +19,13 @@ document.addEventListener('DOMContentLoaded', function () {
   showSlide(currentIndex);
 });
 
-// let currentIndex = 0;
-//   const items = document.querySelector('.homepage-img-container');
-//   const totalItems = document.querySelectorAll('.homepage-img').length;
 
-//   function showNext() {
-//     if (currentIndex < totalItems - 1) {
-//       currentIndex++;
-//     } else {
-//       currentIndex = 0;
-//     }
-//     updateCarousel();
-//   }
+// hamburger menu or slide over menu
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".slide-over-menu-list");
 
-//   function updateCarousel() {
-//     const newTransformValue = -currentIndex * 100 + '%';
-//     items.style.transform = 'translateX(' + newTransformValue + ')';
-//   }
+hamburger.addEventListener("click", () => {
+    // hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-//   setInterval(showNext, 3000); // Change image every 3 seconds (adjust as needed)
